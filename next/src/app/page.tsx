db8f30ce7242b34bc06a999e9985c6e2e4415bd7
feliche93 <felix.vemmer@gmail.com>
2023-10-16 10:35:50 +0200

    feat: shadcn components

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import { db } from '../lib/db';
+import { Button } from '@/components/ui/button';
 
 export default async function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Hello
-    </main>
+    <>
+      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+        Hello
+      </main>
+    </>
   );
 }
