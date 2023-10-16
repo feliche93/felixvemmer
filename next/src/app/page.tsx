7204730e06c2cbdab9a960cb1264241bfe2e5f89
feliche93 <felix.vemmer@gmail.com>
2023-10-16 13:29:17 +0200

    chore working page header

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link'
 
-import { Icons } from '@/components/icons'
 import { PageHeader, PageHeaderDescription, PageHeaderHeading } from '@/components/page-header'
 import { buttonVariants } from '@/components/ui/button'
-import { siteConfig } from '@/config/site'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
 
 export default function IndexPage() {
   return (
     <div className="container relative">
-      <PageHeader className="pb-8">
+      <PageHeader className="pb-8 flex flex-col items-center">
         {/* <Link
           href="/docs/changelog"
           className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
@@ -19,23 +18,39 @@ export default function IndexPage() {
           <span className="hidden sm:inline">Introducing Style, a new CLI and more.</span>
           <ArrowRightIcon className="ml-1 h-4 w-4" />
         </Link> */}
-        <PageHeaderHeading>Hey, I am Felix 👋 Welcome to my personal website.</PageHeaderHeading>
-        <PageHeaderDescription>
-          You will find straight-to-the-point insights and practical advice on Python, TypeScript,
-          and all things full-stack.
+        <Image
+          className="border border-primary-foreground rounded-full"
+          src={'/logos/felix-vemmer.png'}
+          priority={true}
+          width={200}
+          height={200}
+          alt="Felix Vemmer"
+        />
+        <PageHeaderHeading>👋 Welcome to my personal website</PageHeaderHeading>
+        <PageHeaderDescription className="text-center">
+          Hey, I am Felix, a full-stack developer based in Berlin. You will find
+          straight-to-the-point insights and practical advice on Python, TypeScript, and all things
+          full-stack.
         </PageHeaderDescription>
-        <div className="flex w-full items-center space-x-4 pb-8 pt-4 md:pb-10">
-          <Link href="/docs" className={cn(buttonVariants())}>
-            Get Started
+        <div className="flex w-full justify-center items-center space-x-4 pb-8 pt-4 md:pb-10">
+          <Link
+            href="/consulting"
+            className={cn(
+              buttonVariants({
+                variant: 'default',
+                size: 'lg',
+              }),
+            )}
+          >
+            Consulting Services
           </Link>
           <Link
             target="_blank"
             rel="noreferrer"
-            href={siteConfig.links.github}
-            className={cn(buttonVariants({ variant: 'outline' }))}
+            href={'/blog'}
+            className={cn(buttonVariants({ variant: 'outline', size: 'lg' }))}
           >
-            <Icons.gitHub className="mr-2 h-4 w-4" />
-            GitHub
+            Blog
           </Link>
         </div>
       </PageHeader>
