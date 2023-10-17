ca2da221eb35f1bdc628eea382163c607b04054d
feliche93 <felix.vemmer@gmail.com>
2023-10-17 23:49:09 +0200

    feat: next.js fixes

--- next/src/components/callout.tsx
+++ next/src/components/callout.tsx
@@ -10,7 +10,7 @@ export function Callout({ title, children, icon, ...props }: CalloutProps) {
   return (
     <Alert {...props}>
       {icon && <p className="pb-2">{icon}</p>}
-      {title && <AlertTitle>{title}</AlertTitle>}
+      {title && <AlertTitle className="py-2 font-semibold">{title}</AlertTitle>}
       <AlertDescription>{children}</AlertDescription>
     </Alert>
   )
