07292c2b6262fe2e95cbbb056307c88454db16d1
feliche93 <felix.vemmer@gmail.com>
2023-10-21 10:39:10 +0200

    feat: adds json ld

--- next/src/app/[locale]/blog/[...slug]/page.tsx
+++ next/src/app/[locale]/blog/[...slug]/page.tsx
@@ -17,6 +17,7 @@ import { FireIcon } from '@heroicons/react/24/outline'
 import { Metadata } from 'next'
 import { getTranslator } from 'next-intl/server'
 import Link from 'next/link'
+import { Article } from 'schema-dts'
 
 interface PostPageProps {
   params: {
@@ -120,73 +121,118 @@ export default async function PostPage({ params }: PostPageProps) {
     notFound()
   }
 
+  const article: Article = {
+    '@type': 'Article',
+    publisher: {
+      '@type': 'Organization',
+      name: 'Felix Vemmer',
+      url: absoluteUrl('/'),
+      logo: {
+        '@type': 'ImageObject',
+        url: 'https://www.404media.co/content/images/2023/08/Logo-Color-on-White@24x.png',
+      },
+    },
+    author: authors.map((author) => ({
+      '@type': 'Person',
+      name: author?.title,
+      image: {
+        '@type': 'ImageObject',
+        url: author?.avatar,
+      },
+      url: absoluteUrl(`/authors/${author?.slug}`),
+      sameAs: [author?.twitter].filter(Boolean) as string[],
+    })),
+    headline: post.title,
+    url: absoluteUrl(post.slug),
+    datePublished: post.date,
+    dateModified: post.date,
+    image: {
+      '@type': 'ImageObject',
+      url: post.image,
+    },
+    keywords: post.categories.join(', '),
+    description: post.description,
+    mainEntityOfPage: absoluteUrl(post.slug),
+  }
+
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    ...article,
+  }
+
   const toc = await getTableOfContents(post.body.raw)
 
   return (
-    <main className="px-4 md:px-8 mx-auto max-w-6xl relative py-6 lg:grid lg:grid-cols-[1fr_300px] md:gap-5 lg:gap-10 lg:py-10 xl:gap-20">
-      <div className="prose prose-p:text-base sm:prose-p:text-lg mx-auto sm:max-w-2xl md:max-w-2xl">
-        <article className="">
-          <div className="flex flex-wrap-reverse items-center gap-4 pb-4">
-            {/* Categories */}
-            <div className="flex gap-1 flex-wrap">
-              {post.categories &&
-                post.categories.map((category, index) => (
-                  <Badge className="" key={index}>
-                    {category}
-                  </Badge>
-                ))}
-            </div>
-            {/* Featured */}
-            {post.featured && (
-              <div className="flex flex-wrap items-center gap-2">
-                <FireIcon className="h-6 w-6 text-destructive" />
-                <span className="text-destructive font-semibold">Featured</span>
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <main className="px-4 md:px-8 mx-auto max-w-6xl relative py-6 lg:grid lg:grid-cols-[1fr_300px] md:gap-5 lg:gap-10 lg:py-10 xl:gap-20">
+        <div className="prose prose-p:text-base sm:prose-p:text-lg mx-auto sm:max-w-2xl md:max-w-2xl">
+          <article className="">
+            <div className="flex flex-wrap-reverse items-center gap-4 pb-4">
+              {/* Categories */}
+              <div className="flex gap-1 flex-wrap">
+                {post.categories &&
+                  post.categories.map((category, index) => (
+                    <Badge className="" key={index}>
+                      {category}
+                    </Badge>
+                  ))}
               </div>
-            )}
-          </div>
-          {/* Title */}
-          <h1 className="text-3xl font-bold tracking-tight sm:text-4xl">{post.title}</h1>
-          {/* Excerpt */}
-          <p className="text-xl pt-4 leading-8">{post.description}</p>
-          {/* Author */}
-          <div className="flex h-24 flex-row items-center gap-x-4">
+              {/* Featured */}
+              {post.featured && (
+                <div className="flex flex-wrap items-center gap-2">
+                  <FireIcon className="h-6 w-6 text-destructive" />
+                  <span className="text-destructive font-semibold">Featured</span>
+                </div>
+              )}
+            </div>
+            {/* Title */}
+            <h1 className="text-3xl font-bold tracking-tight sm:text-4xl">{post.title}</h1>
+            {/* Excerpt */}
+            <p className="text-xl pt-4 leading-8">{post.description}</p>
+            {/* Author */}
+            <div className="flex h-24 flex-row items-center gap-x-4">
+              <Image
+                priority={true}
+                width={56}
+                height={56}
+                alt={authors[0].title}
+                src={authors[0].avatar}
+              />
+              <div className="flex flex-col gap-1 text-sm">
+                <span className="text-lg font-bold">{authors[0].title}</span>
+                <span>{formatDate(post.date)}</span>
+              </div>
+            </div>
+            {/* Cover Image */}
             <Image
               priority={true}
-              width={56}
-              height={56}
-              alt={authors[0].title}
-              src={authors[0].avatar}
+              alt={post.title}
+              width={804}
+              height={452}
+              className="rounded-md border my-4"
+              src={post.image}
             />
-            <div className="flex flex-col gap-1 text-sm">
-              <span className="text-lg font-bold">{authors[0].title}</span>
-              <span>{formatDate(post.date)}</span>
-            </div>
+            {/* Content */}
+            <Mdx code={post.body.code} />
+          </article>
+          <hr className="my-8" />
+          <div className="flex justify-center py-6 lg:py-10">
+            <Link href="/blog" className={cn(buttonVariants({ variant: 'ghost' }))}>
+              <Icons.chevronLeft className="mr-2 h-4 w-4" />
+              See all posts
+            </Link>
           </div>
-          {/* Cover Image */}
-          <Image
-            priority={true}
-            alt={post.title}
-            width={804}
-            height={452}
-            className="rounded-md border my-4"
-            src={post.image}
-          />
-          {/* Content */}
-          <Mdx code={post.body.code} />
-        </article>
-        <hr className="my-8" />
-        <div className="flex justify-center py-6 lg:py-10">
-          <Link href="/blog" className={cn(buttonVariants({ variant: 'ghost' }))}>
-            <Icons.chevronLeft className="mr-2 h-4 w-4" />
-            See all posts
-          </Link>
         </div>
-      </div>
-      <div className="hidden text-sm lg:block">
-        <div className="sticky top-16 -mt-10 max-h-[calc(var(--vh)-4rem)] overflow-y-auto pt-10">
-          <DashboardTableOfContents title={t('tocHeading')} toc={toc} />
+        <div className="hidden text-sm lg:block">
+          <div className="sticky top-16 -mt-10 max-h-[calc(var(--vh)-4rem)] overflow-y-auto pt-10">
+            <DashboardTableOfContents title={t('tocHeading')} toc={toc} />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
