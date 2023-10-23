2b9a14d80346e9a7f91121f28cf9e2fe03f527b4
feliche93 <felix.vemmer@gmail.com>
2023-10-23 11:47:33 +0200

    fix middleware to skip ingest

--- next/src/middleware.ts
+++ next/src/middleware.ts
@@ -12,5 +12,5 @@ export default createMiddleware({
 export const config = {
   // Skip all paths that should not be internationalized. This example skips
   // certain folders and all pathnames with a dot (e.g. favicon.ico)
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: ['/((?!api|_next|_vercel|ingest|.*\\..*).*)'],
 }
