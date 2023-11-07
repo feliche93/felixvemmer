86bebe57ceaa804366f04d5209ed1659500bd617
feliche93 <felix.vemmer@gmail.com>
2023-11-07 14:55:43 +0100

    feat: translations

--- next/src/app/[locale]/consulting-services/page.tsx
+++ next/src/app/[locale]/consulting-services/page.tsx
@@ -1,4 +1,3 @@
-import imageLaptop from '@/../public/images/laptop.jpg'
 import imageMeeting from '@/../public/images/meeting.jpg'
 import imageWhiteboard from '@/../public/images/whiteboard.jpg'
 import { Blockquote } from '@/components/ui/blockquote'
@@ -50,140 +49,49 @@ function Section({
 }
 
 function Discover() {
+  const t = useTranslations('consultingServices.discover')
   return (
-    <Section title="Discovery Call" image={{ src: imageWhiteboard }}>
-      <div className="space-y-4 text-base text-muted-foreground">
-        <p>
-          Transforming your business ideas into digital success is what I do best. My
-          <strong className="font-semibold text-foreground">
-            {' '}
-            CEMS Master's in International Management{' '}
-          </strong>
-          and deep technical knowledge enable me to turn complex challenges into
-          <strong className="font-semibold text-foreground"> effective tech solutions</strong>. I'm
-          committed to
-          <strong className="font-semibold text-foreground"> clear communication</strong>, ensuring
-          your goals are achieved with custom solutions tailored for your business.
-        </p>
-
-        <p>
-          Our discovery call will be a session of
-          <strong className="font-semibold text-foreground"> honest feedback </strong> and
-          <strong className="font-semibold text-foreground"> actionable insights</strong>. With a
-          lean MVP approach, I prioritize a fast and focused launch, attuned to your customers'
-          needs and your project's unique requirements.
-        </p>
-
-        <p>
-          Transparency is a given with me. If a project stretches beyond my expertise, I'll be
-          upfront about it. But with my robust knowledge of the
-          <strong className="font-semibold text-foreground"> React and Python </strong> ecosystems,
-          I'm ready to craft a
-          <strong className="font-semibold text-foreground">
-            {' '}
-            comprehensive full-stack strategy{' '}
-          </strong>{' '}
-          for your project's success.
-        </p>
+    <Section title={t('title')} image={{ src: imageWhiteboard }}>
+      <div className="space-y-6 text-base text-muted-foreground">
+        {t.rich('description', {
+          strong: (chunks) => <strong className="font-semibold text-foreground">{chunks}</strong>,
+          p: (chunks) => <p>{chunks}</p>,
+        })}
       </div>
-
       <Blockquote
         author={{ name: 'Julian Richter', role: 'Founder at DecentralizedFinance.com' }}
         className="mt-12"
       >
-        I collaborated with Felix on a project for DezentralizedFinance.com to create a scalable
-        logo map SaaS tool. Felix was professional, timely, and efficient. He excelled in
-        communication, adapting to my vision of integrating a prototype with Google Sheets. What
-        stood out was Felix's understanding of my workflow and needs.
+        {t('quote')}
       </Blockquote>
-
-      {/* <h3 className="mt-12 font-display text-base font-semibold text-foreground">
-        Included in this phase
-      </h3>
-      <TagList className="mt-4">
-        <TagListItem>In-depth questionnaires</TagListItem>
-        <TagListItem>Feasibility studies</TagListItem>
-        <TagListItem>Proofs-of-concept</TagListItem>
-      </TagList> */}
     </Section>
   )
 }
 
 function Offer() {
+  const t = useTranslations('consultingServices.offer')
   return (
-    <Section title="Offer" image={{ src: imageLaptop, shape: 1 }}>
+    <Section title={t('title')} image={{ src: imageWhiteboard }}>
       <div className="space-y-6 text-base text-muted-foreground">
-        <p>
-          Post-discovery, you'll receive a{' '}
-          <strong className="font-semibold text-foreground">tailored proposal</strong>, complete
-          with{' '}
-          <strong className="font-semibold text-foreground">
-            minimum and maximum pricing brackets
-          </strong>
-          . This gives you a{' '}
-          <strong className="font-semibold text-foreground">
-            transparent view of potential costs
-          </strong>
-          , ensuring no surprises down the line.
-        </p>
-        <p>
-          Understanding that every project is unique, I offer the{' '}
-          <strong className="font-semibold text-foreground">flexibility to adjust the scope</strong>{' '}
-          to meet your financial and project requirements. We can work together to align the
-          project's needs with your budget, crafting a solution that's{' '}
-          <strong className="font-semibold text-foreground">right for you</strong>.
-        </p>
-        <p>
-          My aim is to deliver{' '}
-          <strong className="font-semibold text-foreground">exceptional value</strong>, balancing
-          quality and cost. By{' '}
-          <strong className="font-semibold text-foreground">refining the project scope</strong>, we
-          can find a sweet spot that{' '}
-          <strong className="font-semibold text-foreground">maximizes your investment</strong> and
-          brings your vision to life without compromise.
-        </p>
+        {t.rich('description', {
+          strong: (chunks) => <strong className="font-semibold text-foreground">{chunks}</strong>,
+          p: (chunks) => <p>{chunks}</p>,
+        })}
       </div>
-
-      {/* <h3 className="mt-12 font-display text-base font-semibold text-foreground">
-        Included in this phase
-      </h3>
-      <TagList className="mt-4">
-        <TagListItem>In-depth questionnaires</TagListItem>
-        <TagListItem>Feasibility studies</TagListItem>
-        <TagListItem>Proofs-of-concept</TagListItem>
-      </TagList> */}
     </Section>
   )
 }
 
 function Deliver() {
+  const t = useTranslations('consultingServices.deliver')
+
   return (
     <Section title="Deliver" image={{ src: imageMeeting, shape: 2 }}>
       <div className="space-y-6 text-base text-muted-foreground">
-        <p>
-          I ensure the solution sticks to the agreed proposal, focusing on essential features to
-          prevent <strong className="font-semibold text-foreground">feature bloat</strong>. However,
-          I stay adaptable to refine the project scope as your needs evolve, keeping things{' '}
-          <strong className="font-semibold text-foreground">simple and efficient</strong>.
-        </p>
-
-        <p>
-          Delivery speed is championed through modern cloud providers like Vercel and Modal.com,
-          which support{' '}
-          <strong className="font-semibold text-foreground">
-            rapid scaling and robust deployment
-          </strong>{' '}
-          of web applications.
-        </p>
-        <p>
-          By leveraging full-stack frameworks like Next.js, combined with clean, well-documented
-          code and robust testing, I deliver products that are not just quickly brought to market,
-          but also{' '}
-          <strong className="font-semibold text-foreground">maintainable and scalable</strong>. My
-          commitment to code quality involves using TypeScript/Python type hints, which enhances
-          readability and future-proofs your application, making it ready for whatever changes lie
-          ahead.
-        </p>
+        {t.rich('description', {
+          strong: (chunks) => <strong className="font-semibold text-foreground">{chunks}</strong>,
+          p: (chunks) => <p>{chunks}</p>,
+        })}
       </div>
 
       <h3 className="mt-12 font-display text-base font-semibold text-foreground">
@@ -289,7 +197,7 @@ export default function Process({ params: { locale } }: { params: { locale: stri
         <Deliver />
       </div>
 
-      <Values />
+      {/* <Values /> */}
 
       {/* <ContactSection /> */}
     </>
