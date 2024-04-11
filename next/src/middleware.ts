421f16f77fc75bd5f8c7d4502563421c16e1589d
feliche93 <felix.vemmer@gmail.com>
2024-04-11 16:12:03 +0200

    fix: changes middleware matcher

--- next/src/middleware.ts
+++ next/src/middleware.ts
@@ -31,5 +31,11 @@ export default authMiddleware({
 })
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|ingest|.*\\..*).*)'],
+  matcher: [
+    // Exclude files with a "." followed by an extension, which are typically static files.
+    // Exclude files in the _next directory, which are Next.js internals.
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    // Re-include any files in the api or trpc folders that might have an extension
+    '/(api|trpc)(.*)',
+  ],
 }
