e9cbc61b094076cbd1ae7df2320c314b60f7f021
feliche93 <felix.vemmer@gmail.com>
2024-04-26 13:23:32 +0200

    chore: testing bailout of rendering

--- next/src/components/auth-clerk-header.tsx
+++ next/src/components/auth-clerk-header.tsx
@@ -1,11 +1,14 @@
 import { Link } from '@/app/navigation'
 import { UserButton } from '@clerk/nextjs'
 import { auth } from '@clerk/nextjs/server'
+import { unstable_noStore } from 'next/cache'
 import { FC } from 'react'
 import { buttonVariants } from './ui/button'
 
 export interface AuthClerkHeaderProps {}
 export const AuthClerkHeader: FC<AuthClerkHeaderProps> = () => {
+  unstable_noStore() // opt out before we even get to the try/catch
+
   const { userId } = auth()
 
   return (
