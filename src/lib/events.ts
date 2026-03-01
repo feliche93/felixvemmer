07a51f33275b3a417f57ff82fe224a29f2bafc7a
feliche93 <felix.vemmer@gmail.com>
2026-03-01 14:22:39 +0100

    fix(zod): update record schema signature for zod v4

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,19 +1,21 @@
 // import va from '@vercel/analytics'
-import { z } from 'zod'
+import { z } from "zod"
 
 const eventSchema = z.object({
   name: z.enum([
-    'copy_npm_command',
-    'copy_usage_import_code',
-    'copy_usage_code',
-    'copy_primitive_code',
-    'copy_theme_code',
-    'copy_block_code',
-    'copy_chunk_code',
-    'enable_lift_mode',
+    "copy_npm_command",
+    "copy_usage_import_code",
+    "copy_usage_code",
+    "copy_primitive_code",
+    "copy_theme_code",
+    "copy_block_code",
+    "copy_chunk_code",
+    "enable_lift_mode",
   ]),
   // declare type AllowedPropertyValues = string | number | boolean | null
-  properties: z.record(z.union([z.string(), z.number(), z.boolean(), z.null()])).optional(),
+  properties: z
+    .record(z.string(), z.union([z.string(), z.number(), z.boolean(), z.null()]))
+    .optional(),
 })
 
 export type Event = z.infer<typeof eventSchema>
